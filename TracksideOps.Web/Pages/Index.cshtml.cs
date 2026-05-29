28e4f16b38310458adcd3d779a71cfeeefc54975
Collin Scherbert <cscherbert1@gmail.com>
2026-05-29 10:41:32 -0500

    initial commit

---
+++ TracksideOps.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TracksideOps.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
